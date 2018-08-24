9b0b40521ede12ab9abc36e0a791f2b9edd79a29
Catero <cybercat_666_78@hotmail.com>
2018-08-24 05:53:19 -0300

    cambiados tabs por espacios

--- fibo/src/main/java/ar/edu/untref/dyasc/Program.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/Program.java
@@ -7,12 +7,12 @@ public class Program {
     public static ArrayList<Integer> fibo(int cantidad) {
         ArrayList<Integer> lista = new ArrayList<Integer>();
         lista.add(0);
-        if(cantidad == 1) {
-        	return lista;
+        if (cantidad == 1) {
+            return lista;
         }
         lista.add(1);
-        if(cantidad == 2) {
-        	return lista;
+        if (cantidad == 2) {
+            return lista;
         }
         for (int i = 2; i < cantidad; i++) {
             lista.add(lista.get(i - 1) + lista.get(i - 2));
@@ -26,7 +26,7 @@ public class Program {
         ArrayList<Integer> resultado = fibo(cantidad);
         System.out.print("fibo<" + cantidad + ">:");
         for (int i = 0; i < cantidad; i++) {
-        	System.out.print(" " + resultado.get(i));
+            System.out.print(" " + resultado.get(i));
         }
     }
 
