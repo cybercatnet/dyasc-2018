6e056ea9afeed6395d59f68e7dd4f32a15bba86c
Catero <cybercat_666_78@hotmail.com>
2018-08-24 04:10:55 -0300

    Se agregó función fibo y su representación

--- Fibo/src/main/java/ar/edu/untref/dyasc/Program.java
+++ Fibo/src/main/java/ar/edu/untref/dyasc/Program.java
@@ -1,11 +1,33 @@
 package ar.edu.untref.dyasc;
 
+import java.util.ArrayList;
+
 public class Program {
 
+    public static ArrayList<Integer> fibo(int cantidad) {
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        lista.add(0);
+        if(cantidad == 1) {
+        	return lista;
+        }
+        lista.add(1);
+        if(cantidad == 2) {
+        	return lista;
+        }
+        for (int i = 2; i < cantidad; i++) {
+            lista.add(lista.get(i - 1) + lista.get(i - 2));
+        }
+        return lista;
+    }
+
     public static void main(String[] args) {
-    
-        System.out.println("Hola a todos!!!");
-    
+
+        int cantidad = Integer.parseInt(args[0]);
+        ArrayList<Integer> resultado = fibo(cantidad);
+        System.out.print("fibo<" + cantidad + ">:");
+        for (int i = 0; i < cantidad; i++) {
+        	System.out.print(" " + resultado.get(i));
+        }
     }
 
 }
